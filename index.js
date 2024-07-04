7004d44ba1719b8244fbc63864174c4cd814fbcc
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-07-04 06:38:16 +0300

    Adds global scope variables using var and const in index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = `bob`;
+const leastFavoriteCustomer = `StinkMeaner`;
+
+function upperCaseCustomerName() {
+    return customerName = `BOB`;
+}
+
+function setBestCustomer() {
+    return bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = `maybe bob`;
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = `Uncle Rukus no relation`;
+}
+
+console.log(customerName.toUpperCase());
